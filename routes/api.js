ff5d280fe36e68a059fc16cacbbc12640ec390f3
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-01-30 00:13:45 -0500

    Got a note to show up in notes section and appends to the db.json file.

--- routes/api.js
+++ routes/api.js
@@ -2,27 +2,28 @@ const fs= require("fs");
 const path = require("path");
 const dbJson = require("../db/db.json")
 const { v4: uuidv4 } = require('uuid');
-//installed random id generator and copied syntax needed to work
+//installed random id generator and copied syntax needed to work from npm website
 
 module.exports = function(app){
     app.get("/api/notes", function(req, res){
-        return res.json(dbJson);
+        // return 
+        res.json(dbJson);
         //dbJson is the array
     })
 
     app.post("/api/notes", function(req, res){
         var data = req.body;
         data.id= uuidv4();
-        //defined the unique id to my code.
+        //defined the unique id to the data of my code.
         //console.log(data);
         //pushing data into array of dbJson
         dbJson.push(data);
-        fs.writeFile(path.join(__dirname, "../db/db.json"),JSON.stringify);
+        fs.writeFile(path.join(__dirname, "../db/db.json"),JSON.stringify
         (dbJson),function(err){
             if(err)throw err
             //if wrong throw error, if not take the response.json and push the new array of data and show the user the information.
             res.json(dbJson);  
-        } 
+        }) 
     })
 }
 console.log(dbJson);
--- routes/html.js
+++ routes/html.js
@@ -1,9 +1,8 @@
 const fs= require("fs");
-
-
-//dirname =absolute path
 const path = require("path");
-module.exports = function(app){
+//dirname =absolute path
+
+module.exports = function(app) {
     // Basic route that sends the user first to the AJAX Page
     app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
