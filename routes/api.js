5caf99db951565465a81ed49487ffd74564eff65
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-01-29 23:12:35 -0500

    Defined the request.body I want to use, pushed the same data to the dbjson file and started to write the post request, and finished the get request.

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,18 @@ const dbJson = require("../db/db.json")
 
 module.exports = function(app){
     app.get("/api/notes", function(req, res){
-        return response.json(dbJson);
+        return res.json(dbJson);
+        
+    })
+
+    app.post("/api/notes", function(req, res){
+        var data = req.body;
+        //console.log(data);
+        //pushing data into array of dbJson
+        dbJson.push(data);
+        fs.writeFile(path.join(__dirname, "../db/db.json"),JSON.stringify);
+        
+        //res.json("Success!");   
     })
 }
+console.log(dbJson);
