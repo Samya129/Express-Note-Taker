4bd298ab4fc8a0acd575dc9b3f0dc94f34553762
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-01-31 20:37:38 -0500

    Added some notes and pseudocode for delete request.

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 const fs= require("fs");
+//need fs to serve the html documents
 const path = require("path");
 const dbJson = require("../db/db.json")
 const { v4: uuidv4 } = require('uuid');
 //installed random id generator and copied syntax needed to work from npm website
-
+//v4...
 module.exports = function(app){
     app.get("/api/notes", function(req, res){
         // return 
@@ -19,11 +20,31 @@ module.exports = function(app){
         //pushing data into array of dbJson
         dbJson.push(data);
         fs.writeFile(path.join(__dirname, "../db/db.json"),JSON.stringify
-        (dbJson),function(err){
-            if(err)throw err
+        (dbJson),function(err, dbJson){
+            //in this case, dbJson is the 'data' array which are the contents inside of the array.
+            if(err)throw err;
             //if wrong throw error, if not take the response.json and push the new array of data and show the user the information.
             res.json(dbJson);  
         }) 
     })
+    app.Delete("/api.notes/id", function (req, res){
+    //var idToDelete = req.params.id
+    const uniqueId = data.id;
+    const result = uniqueId.filter(deleteNote)
+    console.log(result)
+    if (dbJson[2] === data.id){
+        //filter out this object and push it
+    }
+
+   
+
+    //read notes, remove one, write it back 
+    // gather the current notes in an array
+    // remove the one that matches id
+    // write the new array back to the file system
+    //map, .find, indexOf, splice, filter?
+
+    })
+
 }
 console.log(dbJson);
--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-
 //Set up an Express Application
 const app = express();
 const PORT = process.env.PORT || 3003
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+//Serve static js files with Express/can be used multiple times
 app.use(express.static("public"));
 require("./routes/api")(app);
 require("./routes/html")(app);
-//Starting the server 
+
+//Starting the server
 app.listen(PORT, function(){
     console.log("it's working" + PORT)
 });
