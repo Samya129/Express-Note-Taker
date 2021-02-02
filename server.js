f7b4b1d86a43ed2b398df696774b55769338f590
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-02 00:10:47 -0500

    Formatted the documents with prettier and deleted unnecessary code.

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const fs = require("fs");
 
 //Set up an Express Application
 const app = express();
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT || 3003;
 
 // Sets up the Express app to handle data parsing(middle-ware)
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//Serve static js files with Express/can be used multiple times
+
+//Serve static js files with Express
 app.use(express.static("public"));
 require("./routes/api")(app);
 require("./routes/html")(app);
 
 //Starting the server
-app.listen(PORT, function(){
-    console.log("it's working" + PORT)
+app.listen(PORT, function () {
+  console.log("it's working" + PORT);
 });
