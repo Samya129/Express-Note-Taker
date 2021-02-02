5e80ad0bea7d315dbc03c53af1dbdbe43f0eba2a
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-02 00:31:20 -0500

    Fixed formatting I was missing in some files.

--- routes/html.js
+++ routes/html.js
@@ -1,14 +1,14 @@
-const fs= require("fs");
+const fs = require("fs");
 const path = require("path");
 //dirname =absolute path
 
-module.exports = function(app) {
-    // Basic route that sends the user first to the AJAX Page
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-      });
-    
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-      });
-}
+module.exports = function (app) {
+  // Basic route that sends the user first to the AJAX Page
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
