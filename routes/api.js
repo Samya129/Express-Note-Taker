f6440a7db4a3099835cb6a4d482e398ded38f0f9
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-19 18:55:07 -0500

    Modified routes api.js file to get saving working properly without delays.

--- routes/api.js
+++ routes/api.js
@@ -10,18 +10,14 @@ module.exports = function (app) {
   });
 
   //POST request
+
   app.post("/api/notes", function (req, res) {
     var newData = req.body;
     newData.id = uuidv4();
     notes.push(newData);
-    fs.writeFile(
-      path.join(__dirname, "../db/db.json"),
-      JSON.stringify(notes),
-      function (err, notes) {
-        if (err) throw err;
-        res.json(notes);
-      }
-    );
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), //pulling the db.json file
+    JSON.stringify(notes))
+    res.json(notes);
   });
 
   //DELETE request
